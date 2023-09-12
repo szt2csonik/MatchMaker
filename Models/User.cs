0fa8bf813cd0adbde4d4e00fb90ad50330fcbd50
Your Name <szt2csonik@vasvari.org>
2023-09-12 11:32:40 +0200

    User class

---
+++ Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public enum GameName { League_of_Legends, CSGO, Valorant, _6_Siege, Other }
+
+namespace MatchMaker.Models
+{
+    public class User
+    {
+        public User()
+        {
+            UserName = string.Empty;
+            BirthsDay = new DateTime();
+            PlayedGame = GameName.Other;
+            AboutMe = string.Empty;
+        }
+
+        public User(string UserName, DateTime birthsDay, GameName schoolClass)
+        {
+            UserName = UserName;
+            BirthsDay = birthsDay;
+            PlayedGame = schoolClass;
+            AboutMe = string.Empty;
+        }
+        public string UserName { get; set; }
+        public DateTime BirthsDay { get; set; }
+        public GameName PlayedGame { get; set; }
+        public string AboutMe { get; set; }
+    }
+}
